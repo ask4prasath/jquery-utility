4bb00d95557e38fe48fd2a7fb5b1fe44baddd32a
unknown <Spritle@.(none)>
2011-05-03 18:30:51 +0530

    adding $.clear and $.isEmpty methods for an array

--- utility.js
+++ utility.js
@@ -12,7 +12,7 @@
 *
 * @example $.number_with_delimeter(1000, ','); => 1,000
 * @desc return the value with delimeters
-
+*/
 
 (function( $ ){
   $.number_with_delimeter = function(number, delimiter) {
@@ -21,5 +21,22 @@
       split[0] = split[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + delimiter);
       return split.join('.');
     }
+	
+   /**
+   * equivalent of Ruby clear 	
+   * Example usage: $.clear([1, 4]); returns a blank array [];   *
+   */ 
+	$.clear = function(array){
+	    array.length = 0;
+		return array;
+	}
+	
+	/**
+	* equivalent of Ruby empty?
+	* Example usage: $.isEmpty(arr); returns a boolean based on whether given array is empty or not.
+	*/
+	$.isEmpty = function(array){
+		return !!(array.length == 0)
+	}
 })( jQuery );
 
